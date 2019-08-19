cd315320e92e3dd5bf8f579ac709edf35ed4570e
stephanides <stefanmacuga1@gmail.com>
2019-08-19 16:29:45 +0200

    unique section

--- pages/index/components/Unique/index.js
+++ pages/index/components/Unique/index.js
@@ -1,6 +1,6 @@
 import './scss/unique.scss';
 import React from 'react';
-import { Container } from 'reactstrap';
+import { Container, Row, Col } from 'reactstrap';
 import PropTypes from 'prop-types';
 import localisation from '../../../../app-data/shared/localisation/Navigation';
 
@@ -10,6 +10,28 @@ const Unique = ({ lang }) => (
       <Container>
         <h2 className="text-center">{localisation[lang].uniqueTitle}</h2>
         <img src="/static/images/unique/karty.png" alt="Vizuál" />
+        <p className="text-center text-p">{localisation[lang].uniqueText}</p>
+        <div className="items_holder">
+          <Row>
+            <Col lg="4">
+              <div className="number_arrow d-flex">
+                <p>01</p>
+                <i className="icono-arrow2-left"></i>
+              </div>
+            </Col>
+            <Col lg="4">
+              <div className="number_arrow d-flex">
+                <p>02</p>
+                <i className="icono-arrow2-left"></i>
+              </div>
+            </Col>
+            <Col lg="4">
+              <div className="number_arrow d-flex">
+                <p >03</p>
+                <i className="icono-arrow2-left"></i>
+              </div></Col>
+          </Row>
+        </div>
       </Container>
     </Container>
   </div>
