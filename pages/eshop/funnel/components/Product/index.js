04b8e579e0a3b404cde26f3d8be6222c65c5fac1
ViktorVojtek <vvojtek@enli.sk>
2019-08-25 19:00:52 +0200

    Work in progress

---
+++ pages/eshop/funnel/components/Product/index.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Col, Row } from 'reactstrap';
+
+const Product = () => (
+  <Row className="mb-5">
+    <Col md="4" lg="4">
+      <aside>
+        <img src="/static/images/LIGHT-PRO.png" alt="" />
+      </aside>
+    </Col>
+    <Col md="8" lg="8">
+      <h3>Sviťte jako profesionál</h3>
+      <p className="font-weight-lighter">
+        Light like pro je návod s přesnými (instrukcemi podobně jako karty CinemaDeck), který vám umožní
+        <br />
+        <strong>profesionálne nasvítit vámi vybranou kompozici</strong>
+        .
+        V balíku LLP najdete celkem 5 světelných scén,
+        <br />
+        5 ruzných nastavení světel (1LS - 5LS), které přímo navazují na karty CinemaDeck.
+      </p>
+      {
+        /* <p>
+          <button></button>
+        </p> */
+      }
+      <div className="price-add-to-cart-container d-flex">
+        <div>
+          <span className="font-weight-bold align-top">7</span>
+          <span className="font-weight-lighter align-top">
+            EUR
+          </span>
+        </div>
+        <button
+          type="button"
+          className="ml-3"
+        >
+          PRIDAT DO KOSIKU
+        </button>
+      </div>
+      <style jsx>
+        {
+          `
+            .price-add-to-cart-container > div > span {
+              font-size: 5rem;
+            }
+            .price-add-to-cart-container > div > span:first-child {
+              line-height: 70px;
+            }
+            .price-add-to-cart-container > div > span:last-child {
+              font-size: 1rem;
+            }
+            .price-add-to-cart-container > button {
+              background-color: #37baf1;
+              border-radius: .25rem;
+              border: 0;
+              color: #fff;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              font-size: .85rem;
+              height: 70px;
+              width: 265px;
+              letter-spacing: .15rem;
+            }
+          `
+        }
+      </style>
+    </Col>
+  </Row>
+);
+
+export default Product;
--- pages/eshop/funnel/index.js
+++ pages/eshop/funnel/index.js
@@ -3,6 +3,7 @@ import { Container } from 'reactstrap';
 import { compose, graphql } from 'react-apollo';
 import { getLocaleQuery, getProductsFromCart } from '../../../app-data/graphql/query';
 import Layout from '../../../app-data/shared/components/Layout';
+import Product from './components/Product';
 
 const Funnel = compose(
   graphql(getLocaleQuery),
@@ -15,25 +16,10 @@ const Funnel = compose(
   >
     <Container>
       <div className="funnel-heading-container">
-        <h2 className="text-uppercase">Poslední dva kroky k dokonalému interview</h2>
+        <h2 className="text-uppercase pt-1 pb-5 mb-5">Poslední dva kroky k dokonalému interview</h2>
       </div>
-      <div>
-        <aside>
-          <img src="/static/images/LIGHT-PRO.png" alt="" />
-        </aside>
-        <div>
-          <h3>Sviťte jako profesionál</h3>
-          <p>
-            Light like pro je návod s přesnými (instrukcemi podobně jako karty CinemaDeck), který vám umožní
-            <br />
-            <strong>profesionálne nasvítit vámi vybranou kompozici</strong>
-            .
-            V balíku LLP najdete celkem 5 světelných scén,
-            <br />
-            5 ruzných nastavení světel (1LS - 5LS), které přímo navazují na karty CinemaDeck.
-          </p>
-        </div>
-      </div>
+      <Product />
+      <Product />
     </Container>
     <style jsx>
       {
