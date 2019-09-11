3c7b706a4bf7a3795d25aaf848de21b8463d828e
stephanides <stefanmacuga1@gmail.com>
2019-09-11 15:33:56 +0200

    email free download

--- pages/index/components/FreeDownload/index.js
+++ pages/index/components/FreeDownload/index.js
@@ -2,7 +2,7 @@
 import './scss/freeDownload.scss';
 import React, { useState } from 'react';
 import {
-  Container, Button, Modal, ModalBody, InputGroup, Input,
+  Container, InputGroup, InputGroupText, Input,
 } from 'reactstrap';
 import PropTypes from 'prop-types';
 import localisation from '../../../../app-data/shared/localisation/FreeDownload';
@@ -31,26 +31,21 @@ const FreeDownload = ({ lang }) => {
               <p className="pl-3">{localisation[lang].freeDownloadText4}</p>
             </div>
           </div>
+          <div className="text-holder">
+            <p>{localisation[lang].email}</p>
+            <InputGroup>
+              <Input placeholder="Email" />
+            </InputGroup>
+            <InputGroupText>
+              <Input addon type="checkbox" aria-label="Checkbox for following text input" />
+              <span>Súhlasím so všeobecnými podmienkami</span>
+            </InputGroupText>
+          </div>
           <button type="button" onClick={() => toggle(!modal)}>{localisation[lang].freeDownloadButton}</button>
           <img className={lang === 'cz' ? 'd-block free-image' : 'd-none free-image'} src="/static/images/freedownload/image.png" alt="" />
           <img className={lang === 'cz' ? 'd-none free-image' : 'd-block free-image'} src="/static/images/freedownload/en/image.png" alt="" />
         </Container>
       </Container>
-      <div>
-        <Modal isOpen={modal} toggle={() => toggle(!modal)}>
-          <ModalBody>
-            <div className="text-holder">
-              <p>{localisation[lang].email}</p>
-              <InputGroup>
-                <Input placeholder="Email" />
-              </InputGroup>
-              <Button type="button" className="modal-button">{localisation[lang].freeDownloadButton}</Button>
-            </div>
-            <img className={lang === 'cz' ? 'd-block free-image' : 'd-none free-image'} src="/static/images/freedownload/image.png" alt="" />
-            <img className={lang === 'cz' ? 'd-none free-image' : 'd-block free-image'} src="/static/images/freedownload/en/image.png" alt="" />
-          </ModalBody>
-        </Modal>
-      </div>
     </div>
   );
 };
