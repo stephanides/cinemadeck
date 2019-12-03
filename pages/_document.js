f1d8ae5588ee6e1104925e1c4c4a0744f2ab2f2f
ViktorVojtek <vvojtek@enli.sk>
2019-12-03 16:51:26 +0100

    Work in progress

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-danger */
 import React from 'react';
 import Document, {
   Html, Head, Main, NextScript,
@@ -16,6 +17,28 @@ class MyDocument extends Document {
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" />
           <meta name="google-site-verification" content="DLioDlInGinWBF6y7FNH6itiz5j-6AiWPyyicqfLXao" />
           <link rel="icon" href="/static/images/favicon.ico" type="image/x-icon" />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `!function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '660729771125279'); 
+            fbq('track', 'PageView');`,
+            }}
+          />
+
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<img height="1" width="1" 
+            src="https://www.facebook.com/tr?id=660729771125279&ev=PageView
+            &noscript=1"/>`,
+            }}
+          />
         </Head>
         <body>
           <Main />
