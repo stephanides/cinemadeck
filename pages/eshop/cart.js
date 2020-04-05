8090ededcc386eeeaeeb7c47d81a3d79f49eebd0
ViktorVojtek <vvojtek@enli.sk>
2020-04-05 22:41:34 +0200

    Added 50% discount

--- pages/eshop/cart.js
+++ pages/eshop/cart.js
@@ -50,6 +50,9 @@ const ShoppingCart = compose(
     if (window.location.href.indexOf('pLyGkyrY7z') > -1) {
       window.localStorage.removeItem('cart');
     }
+    if (window.location.href.indexOf('nzf4dWvJku') > -1) {
+      window.localStorage.removeItem('cart');
+    }
 
     const checkCart = async () => {
       try {
